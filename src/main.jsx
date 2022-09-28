26dfaca255244888ed25ad4006c00ff766b95007
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-09-28 14:06:50 -0300

    Resolved Conflict

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App'
-<<<<<<< HEAD
 import { theme } from './theme/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
->>>>>>> 89f27e7100202c6be74de1fd27876005bd3736e0
       <App />
     </ChakraProvider>
   </React.StrictMode>
