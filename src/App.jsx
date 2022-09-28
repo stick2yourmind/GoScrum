e8542ad6ce5f36b3c5b0e28f91dab5be06b3f349
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-09-28 17:44:47 +0200

    Merge changes from main to dev (#4)
    * add: Login component with validation
    
    add: Login component with validation
    
    * fix: typo at password input
    
    fix: typo
    fix: type password at input
    
    * add: Register mockup
    
    add: Register mockup
    
    * Update .gitignore
    
    Co-authored-by: Max Saravia Moreira <saravia.max.stk2@gmail.com>
    Co-authored-by: Max Saravia Moreira <65754862+stick2yourmind@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import Login from './components/Login'
+import Register from './components/Register'
 
 function App() {
   return (
     <div>
-      <h1>GO SCRUM</h1>
+      <Register />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
