5991c7b1de2709d1f3dab4c195b9f9b42c7dab61
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-09-28 13:36:24 -0300

    Added Homepage, theme and navbar

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+
+const HomeScreen = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>HomeScreen</h1>
+    </>
+  )
+}
+
+export default HomeScreen
