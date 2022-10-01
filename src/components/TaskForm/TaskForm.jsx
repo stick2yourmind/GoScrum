e55c5c3e0552478e8d69c767d35622fa99b4b090
José Gregorio Aguilar D'Vicente <jgglobal12@gmail.com>
2022-10-01 17:57:24 -0400

    Task form (#14)
    * add: Login component with validation
    
    add: Login component with validation
    
    * fix: typo at password input
    
    fix: typo
    fix: type password at input
    
    * add: Register mockup
    
    add: Register mockup
    
    * Update .gitignore
    
    * TaskForm created with active validation
    
    * Aadded task form component to Home Page
    
    * Fix button of the taskForm
    
    * Fix width of the button of TaskForm
    
    Co-authored-by: Max Saravia Moreira <saravia.max.stk2@gmail.com>
    Co-authored-by: Max Saravia Moreira <65754862+stick2yourmind@users.noreply.github.com>

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,67 +1,113 @@
-import { Field, Formik } from 'formik'
-import { Box, Input, Text, Flex, Heading, Button, Select, FormLabel, Textarea, Stack } from '@chakra-ui/react'
-import { useDispatch } from 'react-redux'
+import { Field, Formik } from "formik";
+import {
+  Box,
+  Input,
+  Text,
+  Flex,
+  Heading,
+  Button,
+  Select,
+  FormLabel,
+  Textarea,
+  Stack,
+} from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
 
-import { TaskSchema, loginInit } from '../../utils/schema/taskForm'
-import { setTasks } from '../../store/slices/tasks'
+import { TaskSchema, loginInit } from "../../utils/schema/taskForm";
+import { setTasks } from "../../store/slices/tasks";
 
 const TaskForm = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   const CustomInputComponent = (props) => (
-    <Input borderRadius="8px" className="form__input" type={props.type && 'text'} {...props} />
-  )
+    <Input
+      borderRadius="8px"
+      className="form__input"
+      type={props.type && "text"}
+      {...props}
+    />
+  );
 
   const handleSubmit = (values) => {
-    dispatch(setTasks(values))
-  }
+    dispatch(setTasks(values));
+  };
 
   return (
     <Flex width="100%">
-      <Box fontSize={16} p={6} rounded="md" w={'lg'} width="100%">
+      <Box fontSize={16} p={6} rounded="md" w={"lg"} width="100%">
         <Heading as="h1" fontSize="24px" mb={15}>
           Crear Tarea
         </Heading>
-        <Formik initialValues={loginInit} validationSchema={TaskSchema} onSubmit={handleSubmit}>
+        <Formik
+          initialValues={loginInit}
+          validationSchema={TaskSchema}
+          onSubmit={handleSubmit}
+        >
           {({ errors, touched, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
               <FormLabel fontSize="15px">Crea tus tareas</FormLabel>
-              <Stack align="center" direction={{ base: 'column', md: 'row' }} width="100%">
+              <Stack
+                align="flex-start"
+                direction={{ base: "column", md: "row" }}
+                width="100%"
+              >
                 <Stack width="100%">
-                  <Field as={CustomInputComponent} name="title" placeholder="Título" type="text" />
+                  <Field
+                    as={CustomInputComponent}
+                    name="title"
+                    placeholder="Título"
+                    type="text"
+                  />
                   {errors.title && touched.title && (
-                    <Box className="error" pt={2}>
+                    <Box className="error">
                       <Text color="tomato">{errors.title}</Text>
                     </Box>
                   )}
                 </Stack>
                 <Stack width="100%">
-                  <Field as={Select} borderRadius="8px" name="state" placeholder="Selecciona un estado">
+                  <Field
+                    as={Select}
+                    borderRadius="8px"
+                    name="state"
+                    placeholder="Selecciona un estado"
+                  >
                     <option value="nueva">Nueva</option>
                     <option value="desarrollando">En desarrollo</option>
                     <option value="finalizada">Finalizado</option>
                   </Field>
 
                   {errors.state && touched.state && (
-                    <Box className="error" pt={2}>
+                    <Box className="error">
                       <Text color="tomato">{errors.state}</Text>
                     </Box>
                   )}
                 </Stack>
                 <Stack width="100%">
-                  <Field as={Select} borderRadius="8px" name="priority" placeholder="Selecciona una prioridad">
+                  <Field
+                    as={Select}
+                    borderRadius="8px"
+                    name="priority"
+                    placeholder="Selecciona una prioridad"
+                  >
                     <option value="alta">Alta</option>
                     <option value="media">Media</option>
                     <option value="baja">Baja</option>
                   </Field>
                   {errors.priority && touched.priority && (
-                    <Box className="error" pt={2}>
+                    <Box className="error">
                       <Text color="tomato">{errors.priority}</Text>
                     </Box>
                   )}
                 </Stack>
               </Stack>
-              <Field as={Textarea} h={140} mt={4} name="description" placeholder="Descripción" type="text" />
+              <Field
+                as={Textarea}
+                h={140}
+                mt={4}
+                name="description"
+                placeholder="Descripción"
+                type="text"
+              />
               {errors.description && touched.description && (
                 <Box className="error" pt={2}>
                   <Text color="tomato">{errors.description}</Text>
@@ -69,16 +115,16 @@ const TaskForm = () => {
               )}
               <Button
                 _hover={{
-                  bg: 'background.300',
-                  color: 'primary.100',
-                  borderColor: 'primary.100',
-                  border: '2px'
+                  bg: "background.300",
+                  color: "primary.100",
+                  borderColor: "primary.100",
+                  border: "2px",
                 }}
                 bg="primary.100"
                 border="2px"
                 borderColor="primary.100"
                 color="background.300"
-                marginTop={2}
+                marginTop={4}
                 size="sm"
                 type="submit"
                 width="80px"
@@ -90,7 +136,7 @@ const TaskForm = () => {
         </Formik>
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
