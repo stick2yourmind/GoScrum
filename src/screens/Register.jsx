22eb4ddf43fcf8e6b26e47f6dfd25d454584ed34
JG-PR01 <jgglobal12@gmail.com>
2022-10-07 23:23:59 -0400

    added: error styles for the inputs

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -90,7 +90,14 @@ const Register = () => {
               <FormLabel mb={0} mt={4}>
                 Nombre de usuario
               </FormLabel>
-              <Field as={Input} name="userName" placeholder="Username" type="text" />
+              <Field
+                as={Input}
+                errorBorderColor="tomato"
+                isInvalid={errors.userName && touched.userName}
+                name="userName"
+                placeholder="Username"
+                type="text"
+              />
               {errors.userName && touched.userName && (
                 <Box>
                   <Text color="tomato">{errors.userName}</Text>
@@ -99,7 +106,14 @@ const Register = () => {
               <FormLabel mb={0} mt={4}>
                 Contraseña
               </FormLabel>
-              <Field as={Input} name="password" placeholder="Password" type="password" />
+              <Field
+                as={Input}
+                errorBorderColor="tomato"
+                isInvalid={errors.password && touched.password}
+                name="password"
+                placeholder="Password"
+                type="password"
+              />
               {errors.password && touched.password && (
                 <Box>
                   <Text color="tomato">{errors.password}</Text>
@@ -108,7 +122,14 @@ const Register = () => {
               <FormLabel mb={0} mt={4}>
                 Email
               </FormLabel>
-              <Field as={Input} name="email" placeholder="Email" type="email" />
+              <Field
+                as={Input}
+                errorBorderColor="tomato"
+                isInvalid={errors.email && touched.email}
+                name="email"
+                placeholder="Email"
+                type="email"
+              />
               {errors.email && touched.email && (
                 <Box>
                   <Text color="tomato">{errors.email}</Text>
@@ -129,7 +150,15 @@ const Register = () => {
               </Flex>
               {hasTeam && (
                 <>
-                  <Field as={Input} mt={4} name="teamID" placeholder="Ingrese id del equipo" type="text" />
+                  <Field
+                    as={Input}
+                    errorBorderColor="tomato"
+                    isInvalid={errors.teamID && touched.teamID}
+                    mt={4}
+                    name="teamID"
+                    placeholder="Ingrese id del equipo"
+                    type="text"
+                  />
                   {errors.teamID && touched.teamID && (
                     <Box>
                       <Text color="tomato">{errors.teamID}</Text>
@@ -142,6 +171,8 @@ const Register = () => {
               </FormLabel>
               <Field
                 as={Select}
+                errorBorderColor="tomato"
+                isInvalid={errors.role && touched.role}
                 placeholder="Selecciona un rol"
                 value={values.role}
                 onBlur={() => setFieldTouched('role', true)}
@@ -163,6 +194,8 @@ const Register = () => {
               </FormLabel>
               <Field
                 as={Select}
+                errorBorderColor="tomato"
+                isInvalid={errors.continent && touched.continent}
                 placeholder="Selecciona un continente"
                 value={values.continent}
                 onBlur={() => setFieldTouched('continent', true)}
@@ -187,6 +220,8 @@ const Register = () => {
                   </FormLabel>
                   <Field
                     as={Select}
+                    errorBorderColor="tomato"
+                    isInvalid={errors.region && touched.region}
                     placeholder="Selecciona una región"
                     value={values.region}
                     onBlur={() => setFieldTouched('region', true)}
