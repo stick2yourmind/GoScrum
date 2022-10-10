2c68716e52d785318e2b06a793231f46ef983733
Max Saravia Moreira <saravia.max.stk2@gmail.com>
2022-10-09 21:39:11 -0300

    update: donate button
    added link to donate button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Spacer, Image, Stack, Button, Text, Box } from '@chakra-ui/react'
+import { Spacer, Image, Stack, Button, Text, Box, Link } from '@chakra-ui/react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Logo from '../../assets/GoScrum.png'
@@ -27,24 +27,30 @@ const Navbar = () => {
     >
       <Image alt="GoScrum Logo" h={{ base: '15px', sm: '20px' }} src={Logo} w={{ base: '50px', sm: '80px' }} />
       <Spacer />
-      <Button
+      <Link
+        isExternal
         _hover={{
           bg: 'white',
           color: 'primary.100',
           border: '2px',
           borderColor: 'primary.100'
         }}
+        alignItems="center"
         bg="primary.100"
         border="2px"
         borderColor="primary.100"
+        borderRadius="md"
         boxShadow="dark-lg"
         color="white"
+        display="flex"
         fontSize={{ base: '12px', sm: '15px' }}
         h={{ base: '25px', sm: '25px' }}
+        href="https://www.paypal.com/donate/?hosted_button_id=YXRU5ZVBBE984"
+        justifyContent="center"
         w={{ base: '35px', sm: '80px' }}
       >
         Donar
-      </Button>
+      </Link>
       <Box display={'flex'} flexDirection={{ base: 'column', md: 'row-reverse' }} gap={{ md: 4 }}>
         <Text>{userName}</Text>
         <Text fontSize={{ base: '10px', md: 'inherit' }}>Tareas Creadas: {tasks.length}</Text>
