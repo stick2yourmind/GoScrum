8a3dcdb5c5b8e7a12b5bfff1d9f1a92820ffd638
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-10-10 21:10:40 +0200

    Skeleton and code refactor (#46)

--- src/components/TaskSection/TaskSection.jsx
+++ src/components/TaskSection/TaskSection.jsx
@@ -1,9 +1,8 @@
-import { Stack, Text, Box, Heading } from '@chakra-ui/react'
+import { Stack, Text } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
-import { useDrop } from 'react-dnd'
 import { useSelector } from 'react-redux'
 
-import Card from '../Card/Card'
+import { CardsContainer } from '../CardsContainer/CardsContainer'
 import FilterForm from '../FilterForm/FilterForm'
 
 const TaskSection = () => {
@@ -15,31 +14,6 @@ const TaskSection = () => {
     priorityFilter: '',
     radioFilter: 'all'
   })
-
-  const [{ canDropNew }, dropNew] = useDrop(() => ({
-    accept: ['card_IN PROGRESS', 'card_FINISHED'],
-    drop: () => ({ name: 'newBox' }),
-    collect: (monitor) => ({
-      canDropNew: monitor.canDrop()
-    })
-  }))
-
-  const [{ canDropIP }, dropInProgress] = useDrop(() => ({
-    accept: ['card_NEW', 'card_FINISHED'],
-    drop: () => ({ name: 'inProgressBox' }),
-    collect: (monitor) => ({
-      canDropIP: monitor.canDrop()
-    })
-  }))
-
-  const [{ canDropFinished }, dropFinished] = useDrop(() => ({
-    accept: ['card_NEW', 'card_IN PROGRESS'],
-    drop: () => ({ name: 'finishedBox' }),
-
-    collect: (monitor) => ({
-      canDropFinished: monitor.canDrop()
-    })
-  }))
 
   const filterTasks = () => {
     let tasks = allTasks
@@ -77,78 +51,9 @@ const TaskSection = () => {
       <FilterForm filter={filter} setFilter={setFilter} />
 
       <Stack direction={{ base: 'column', md: 'row' }}>
-        <Box
-          ref={dropNew}
-          bg="white"
-          border="2px"
-          borderColor={canDropNew ? '#08FF08' : 'transparent'}
-          boxShadow="2xl"
-          flex={1}
-          overflow="hidden"
-          paddingBottom={10}
-          paddingTop={2}
-          paddingX={2}
-          rounded="xl"
-        >
-          <Heading fontSize="2xl">Nuevas</Heading>
-          {tasksToShow.length > 0
-            ? tasksToShow.map((task) => {
-                if (task.status === 'NEW') {
-                  return <Card key={task._id} task={task} />
-                }
-
-                return null
-              })
-            : null}
-        </Box>
-        <Box
-          ref={dropInProgress}
-          bg="white"
-          border="2px"
-          borderColor={canDropIP ? '#08FF08' : 'transparent'}
-          boxShadow="2xl"
-          flex={1}
-          height="100%"
-          paddingBottom={10}
-          paddingTop={2}
-          paddingX={2}
-          rounded="xl"
-        >
-          <Heading fontSize="2xl">En Proceso</Heading>
-          {tasksToShow.length > 0
-            ? tasksToShow.map((task) => {
-                if (task.status === 'IN PROGRESS') {
-                  return <Card key={task._id} task={task} />
-                }
-
-                return null
-              })
-            : null}
-        </Box>
-        <Box
-          ref={dropFinished}
-          bg="white"
-          border="2px"
-          borderColor={canDropFinished ? '#08FF08' : 'transparent'}
-          boxShadow="2xl"
-          flex={1}
-          height="100%"
-          paddingBottom={10}
-          paddingTop={2}
-          paddingX={2}
-          rounded="xl"
-        >
-          <Heading fontSize="2xl">Finalizadas</Heading>
-          {tasksToShow.length > 0
-            ? tasksToShow.map((task) => {
-                if (task.status === 'FINISHED') {
-                  return <Card key={task._id} task={task} />
-                }
-
-                return null
-              })
-            : null}
-        </Box>
+        <CardsContainer status="new" tasks={tasksToShow.filter((task) => task.status === 'NEW')} />
+        <CardsContainer status="inProgress" tasks={tasksToShow.filter((task) => task.status === 'IN PROGRESS')} />
+        <CardsContainer status="finished" tasks={tasksToShow.filter((task) => task.status === 'FINISHED')} />
       </Stack>
     </Stack>
   )
